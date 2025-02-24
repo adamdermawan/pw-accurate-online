1a3cdf319d2eedcb4a2654b4225b09ec4f29ab9f
adamdrm <adamdermawan535@gmail.com>
2025-02-24 20:24:49 +0700

    can create a sales invoice transaction until it's finish

--- tests/createTransaction.spec.ts
+++ tests/createTransaction.spec.ts
@@ -5,7 +5,7 @@ import { test, expect, BrowserContext, Page } from '@playwright/test';
  * so ignore first if the test feels so flaky
  */
 
-const URL = "...";
+const URL = "https://account.accurate.test1";
 
 test.beforeEach(async ({page}) => {
   test.setTimeout(120_000);
@@ -14,9 +14,9 @@ test.beforeEach(async ({page}) => {
 
 test('Login AOL', async ({ page, context }) => {
   //Login to Accurate Online
-  await page.locator("//form[@id='login-form']//input[@id='account']").fill("...");
-  await page.locator("//form[@id='login-form']//input[@id='password']").fill("...");
-  await page.locator("//button[@id='btn-login']").click();
+  await page.locator("//form[@id='login-form']//input[@id='account']").fill("anisalt@bisnis2030.com");
+  await page.locator("//form[@id='login-form']//input[@id='password']").fill("123456");
+  await page.getByRole('button', { name: 'Masuk' }).click();
 
   //Login Success
   await expect(page).toHaveTitle(/Data Usaha /);
@@ -26,7 +26,6 @@ test('Login AOL', async ({ page, context }) => {
       page.waitForEvent('popup'),
       page.locator("//h3[.='Airn skincare']").click(),
   ]);
-  //await expect(newTab).toHaveTitle(/Pengaturan Data Usaha ACCURATE/);
 
   //Open sales module
   await newTab.locator(
@@ -40,9 +39,26 @@ test('Login AOL', async ({ page, context }) => {
   ).click();
 
   //create sales invoice
+  /**
+   * I know the code is ugly as fuck, I will improve it in the next commit maybe
+   * but it works for now tho..
+   */
+
+  /* Fill Customer */
   await newTab.getByPlaceholder('Cari/Pilih Pelanggan...').fill('CV Garda');
+  await newTab.locator(
+      "//div[@class='form-content']//form[@class='form']/div[@class='grid fluid no-margin']//button[@class='btn-search']"
+      ).click();
+  await newTab.locator("//div[div[input[@name='customer']]]/ul[@class='lookup-search-result']/li[1]").click();
+
+  /* Fill Item */
   await newTab.getByPlaceholder('Cari/Pilih Barang & Jasa...').fill('air mineral');
+  await newTab.locator(
+      "//div[@class='trans-detail-tab frame-left-tab frames']/div[@class='frame active']//div[@class='span4']//button[@class='btn-search']"
+  ).click();
+  await newTab.locator("//div[div[input[@name='searchDetailItem']]]/ul[@class='lookup-search-result']/li[1]").click();
   await newTab.getByRole('button', { name: 'Lanjut' }).click();
+  //click save file
   await newTab.locator("//*[@id='module-accurate__customer__sales-invoice']//*[@id='btnSave']").click();
 
   //click continue notifications
